9f31ada141ff4e3240f6ac96d920ee624182bc5c
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-08-08 18:05:26 +0200

    feat(cli): add basic bubbletea ingress control flow
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

---
+++ internal/app.go
@@ -0,0 +1,22 @@
+package internal
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Model struct {
+
+}
+
+func KaliInitializer() Model {
+	return Model{ }
+}
+
+func (m Model) Update(message tea.Msg) {
+
+}
+
+func (m Model) View() {
+
+}
+
